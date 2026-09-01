e2e2d9c77127c66749441eea259d54648a3d6595
pankaj sahu <pankaj.sahu@infoaxon.com>
2026-09-01 15:38:35 +0530

    first commit by pankaj not rajkumafgbhnjrjhgfg

---
+++ dist-1sept2026/assets/BackdropLoader-cBIMPpIR.js
@@ -0,0 +1,1 @@
+import{j as a,aE as o,d as s,at as e,T as p}from"./index-BgzlGWt2.js";const d=({loading:r})=>a.jsx(o,{className:"app-backdrop-loader",open:r,children:a.jsxs(s,{className:"app-backdrop-loader__content",children:[a.jsx(e,{color:"inherit"}),a.jsx(p,{variant:"h6",className:"app-backdrop-loader__text",children:"Loading..."})]})});export{d as B};
